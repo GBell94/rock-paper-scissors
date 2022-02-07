9413b5384c319f967a230b9fefc126688b047829
GBell94 <gmb1294@gmail.com>
2022-02-07 22:17:28 +0000

    Add JS functions for getting results and resetting game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,82 @@
+let playerScore = 0;
+let computerScore = 0;
+let resultDiv = document.getElementById('result');
+let selectionsDiv = document.getElementById('selections');
+
+
+getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+    let computerChoice = options[randomNumber];
+    console.log(computerChoice);
+}
 
 let buttons = document.getElementsByTagName('button');
-
 for (let button of buttons) {
     button.addEventListener ('click', function() {
         if (this.getAttribute('data-type') === 'reset') {
             resetGame();
         } else {
             let playerChoice=this.getAttribute('data-type');
-            //playGame(playerChoice);
+            //getResult(playerChoice, computerChoice);
             console.log(playerChoice);
         }
     });
+}
+
+function getResult(playerChoice, computerChoice) {
+    switch (playerChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+        win()
+        break;
+
+        case "scissorsrock":
+        case "rockpaper":
+        case "paperscissors":
+        lose()
+        break;
+
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        draw()
+        break;
+    }
+}
+
+function win(playerChoice, computerChoice) {
+    playerScore++;
+    document.getElementById('player-score').innerHTML = playerScore;
+    selectionsDiv.innerHTML = `
+    <p>You chose: ${playerChoice}</p>
+    <p>Computer chose: ${computerChoice}</p>
+    `;
+    resultDiv.innerHTML = `<p>You Win!</p>`;
+}
+
+function lose(playerChoice, computerChoice) {
+    computerScore++;
+    document.getElementById('computer-score').innerHTML = computerScore;
+    selectionsDiv.innerHTML = `
+    <p>You chose: ${playerChoice}</p>
+    <p>Computer chose: ${computerChoice}</p>
+    `;
+    resultDiv.innerHTML = `<p>Computer Wins ...</p>`;
+}
+function draw(playerChoice, computerChoice) {
+    selectionsDiv.innerHTML = `
+    <p>You chose: ${playerChoice}</p>
+    <p>Computer chose: ${computerChoice}</p>
+    `;
+    resultDiv.innerHTML = `<p>It's a draw</p>`;
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('player-score').innerHTML = playerScore;
+    document.getElementById('computer-score').innerHTML = computerScore;
 }
