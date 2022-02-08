a9a3bbf264b754fb37cc29bdffbdc4b683a3839a
GBell94 <gmb1294@gmail.com>
2022-02-08 19:40:03 +0000

    Change getComputerChoice function to create random computer choice and add choices to selections div and result div

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@ let playerScore = 0;
 let computerScore = 0;
 const resultDiv = document.getElementById('result');
 const selectionsDiv = document.getElementById('selections');
-const computerChoice = getComputerChoice();
+//const computerChoice = getComputerChoice();
+const options = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"];
-    let randomNumber = Math.floor(Math.random() * 3);
-    return options[randomNumber];
-}
+    //let options = ["rock", "paper", "scissors"];
+    //let randomNumber = Math.floor(Math.random() * 3);
+    //return options[randomNumber]; 
+    const randomChoice = options[Math.floor(Math.random() * options.length)];
+    computerChoice = randomChoice;
+} 
 
 let buttons = document.getElementsByTagName('button');
 for (let button of buttons) {
@@ -17,7 +20,12 @@ for (let button of buttons) {
             resetGame();
         } else {
             let playerChoice=this.getAttribute('data-type');
+            getComputerChoice();
             getResult(playerChoice, computerChoice);
+            selectionsDiv.innerHTML = `
+            <p>You chose: ${playerChoice}</p>
+            <p>Computer chose: ${computerChoice}</p>
+            `;
             //console.log(playerChoice);
             //console.log(computerChoice);
         }
@@ -49,28 +57,28 @@ function getResult(playerChoice, computerChoice) {
 function win(playerChoice, computerChoice) {
     playerScore++;
     document.getElementById('player-score').innerHTML = playerScore;
-    selectionsDiv.innerHTML = `
-    <p>You chose: ${playerChoice}</p>
-    <p>Computer chose: ${computerChoice}</p>
-    `;
+   //selectionsDiv.innerHTML = `
+   // <p>You chose: ${playerChoice}</p>
+    //<p>Computer chose: ${computerChoice}</p>
+    //`;
     resultDiv.innerHTML = `<p>You Win!</p>`;
 }
 
 function lose(playerChoice, computerChoice) {
     computerScore++;
     document.getElementById('computer-score').innerHTML = computerScore;
-    selectionsDiv.innerHTML = `
-    <p>You chose: ${playerChoice}</p>
-    <p>Computer chose: ${computerChoice}</p>
-    `;
+   // selectionsDiv.innerHTML = `
+   // <p>You chose: ${playerChoice}</p>
+    //<p>Computer chose: ${computerChoice}</p>
+   // `;
     resultDiv.innerHTML = `<p>Computer Wins ...</p>`;
 }
 
 function draw(playerChoice, computerChoice) {
-    selectionsDiv.innerHTML = `
-    <p>You chose: ${playerChoice}</p>
-    <p>Computer chose: ${computerChoice}</p>
-    `;
+    //selectionsDiv.innerHTML = `
+    //<p>You chose: ${playerChoice}</p>
+    //<p>Computer chose: ${computerChoice}</p>
+    //`;
     resultDiv.innerHTML = `<p>It's a draw</p>`;
 }
 
